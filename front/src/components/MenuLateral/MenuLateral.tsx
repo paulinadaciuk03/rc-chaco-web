c620bafc0d5a909b385159dd9d7f5f88f856a5db
Frankelevich <franco.limberti0308@gmail.com>
2025-04-25 16:14:27 -0300

    prueba

--- front/src/components/MenuLateral/MenuLateral.tsx
+++ front/src/components/MenuLateral/MenuLateral.tsx
@@ -13,6 +13,7 @@ function MenuLateral() {
           <li className="mb-2">Servicios a los socios</li>
           <li className="mb-2">Cursos</li>
           <li className="mb-2">Contactos</li>
+          <li>hola</li>
         </ul>
       </div>
     </>
