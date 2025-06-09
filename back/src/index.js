ae5bca8c46b940ef851e998956e50a7c3f6239de
Paulina Daciuk <paulinadaciukmarinoff@gmail.com>
2025-06-09 16:39:09 -0300

    port

--- back/src/index.js
+++ back/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app/app');
 require('dotenv').config();
-const port = 10000;
+const port = process.env.PORT || 10000;
 
 app.listen(port, () => {
     console.log(`server andando en puerto ${port}`)
