ca1c26b94077f9f2a43e5b77dd9a8b46bc602c0a
Paulina Daciuk <paulinadaciukmarinoff@gmail.com>
2025-06-09 15:48:18 -0300

    url

--- front/src/api/axiosConfig.ts
+++ front/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3001/api/v1", 
+  baseURL: 'https://rc-chaco-web.onrender.com/api/v1', 
   headers: {
     "Content-Type": "application/json",
   },
